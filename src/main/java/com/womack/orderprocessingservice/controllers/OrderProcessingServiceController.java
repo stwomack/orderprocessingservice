6d96ac943962e1563ee143d7344701a29cdff522
Steve Womack <stwomack@gmail.com>
2025-02-27 16:30:13 -0600

    Switched to gui with button push, instead of Scheduled. This service now allows me to scale workers without starting new Scheduled tasks

---
+++ src/main/java/com/womack/orderprocessingservice/controllers/OrderProcessingServiceController.java
@@ -0,0 +1,63 @@
+package com.womack.orderprocessingservice.controllers;
+
+import com.womack.orderprocessingservice.domain.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+public class OrderProcessingServiceController {
+    private static final Random random = new Random();
+    private final Logger LOG = LoggerFactory.getLogger(OrderProcessingServiceController.class);
+
+    @PostMapping("/processPayment")
+    public String processPayment(@RequestBody Payment payment) throws Exception {
+        LOG.info("Processing Payment");
+        if (shouldSucceed()) {
+            return "Payment Successful";
+        } else {
+            throw new Exception("Customer is too broke");
+        }
+    }
+
+    @PostMapping("/checkInventory")
+    public String checkInventory(@RequestBody List<OrderItem> orderItems) throws Exception {
+        LOG.info("Checking Inventory");
+        if (shouldSucceed()) {
+            return "Inventory Check Passed";
+        } else {
+            throw new Exception("We don't got no more of them");
+        }
+    }
+
+    @PostMapping("/shipPackage")
+    public String shipPackage(@RequestBody List<OrderPackage> orderPackages) throws Exception {
+        LOG.info("Shipping Package");
+        if (shouldSucceed()) {
+            return "Package Shipped Successfully";
+        } else {
+            throw new Exception("Package fell off truck");
+        }
+    }
+
+    @PostMapping("/notifyCustomer")
+    public String notifyCustomer(@RequestBody Customer customer) throws Exception {
+        LOG.info("Notifying Customer");
+        if (shouldSucceed()) {
+            return "Customer Successfully Notified";
+        } else {
+            throw new Exception("Customer blocked our notifications");
+        }
+    }
+
+    private boolean shouldSucceed() {
+        int randomNumber = random.nextInt(100);
+        return randomNumber < 80;
+    }
+}
---
+++ src/main/java/com/womack/orderprocessingservice/controllers/OrderSubmissionController.java
@@ -0,0 +1,25 @@
+package com.womack.orderprocessingservice.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class OrderSubmissionController {
+    private final Logger LOG = LoggerFactory.getLogger(OrderSubmissionController.class);
+
+    @Value("${ordersworkflow-url}")
+    private String serviceUrl;
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    @PostMapping("/submitOrder")
+    public String submitOrder() {
+        LOG.error("URL: {}", serviceUrl);
+        restTemplate.postForEntity(serviceUrl + "submitOrder", null, String.class);
+        return "redirect:/";
+    }
+}
